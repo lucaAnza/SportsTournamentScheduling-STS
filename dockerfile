0db5f340a107d841c1f4866a50a91a3fed9604af
Luca Anzaldi <anzaldi.luca@gmail.com>
2025-08-26 18:13:50 +0200

    Setting CP on docker

--- dockerfile
+++ dockerfile
@@ -2,12 +2,9 @@
 FROM python:3.11
 RUN pip install --no-cache-dir z3-solver numpy
 
-# Install MiniZinc binaries
-RUN apt-get update && apt-get install -y wget \
-    && wget https://github.com/MiniZinc/MiniZincIDE/releases/download/2.8.5/MiniZincIDE-2.8.5-bundle-linux-x86_64.tgz \
-    && tar -xvzf MiniZincIDE-2.8.5-bundle-linux-x86_64.tgz -C /opt \
-    && ln -s /opt/MiniZincIDE-2.8.5-bundle-linux-x86_64/bin/minizinc /usr/local/bin/minizinc \
-    && rm MiniZincIDE-2.8.5-bundle-linux-x86_64.tgz
+# Install MiniZinc 
+RUN apt-get update && apt-get install -y minizinc
+RUN apt-get install -y bc
 
 # Where the command will be executed. # Usually is used /app as a convention
 WORKDIR /app   
--- source/CP/start_CP.sh
+++ source/CP/start_CP.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 # Absolute path to THIS script's folder (e.g., .../SAT)
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" 
 while true; do
     echo -e "\n===== Program Menu ====="
     echo "1. Model1 (CP) "
@@ -20,10 +19,11 @@ while true; do
         read -rp "Enter number of teams (int): " team
         
         start=$(date +%s.%N)
-        minizinc -D "n=$team" STS_matchVar_first\ column.mzn > output.txt
+        minizinc -D "n=$team" $SCRIPT_DIR/STS_matchVar_first\ column.mzn > output.txt
         end=$(date +%s.%N)
         runtime=$(echo "$end - $start" | bc)
-        python3 minizinc_reader.py $runtime
+        python3 $SCRIPT_DIR/minizinc_reader.py $runtime
+        rm output.txt
     else
         echo "Invalid option. Try again."
     fi
