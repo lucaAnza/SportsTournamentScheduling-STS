1a8ca7b803978b164c299a528dbcca6205f374ea
Luca Anzaldi <anzaldi.luca@gmail.com>
2026-01-29 11:38:52 +0100

    Add CP chuffed version

--- dockerfile
+++ dockerfile
@@ -2,8 +2,27 @@
 FROM python:3.11
 RUN pip install --no-cache-dir z3-solver numpy mip
 
-# Install MiniZinc 
+# Install MiniZinc ()
 RUN apt-get update && apt-get install -y minizinc
+
+# Install MiniZinc bundle (includes Chuffed) + bc  (COMMENT THIS IF YOU WANT TO TEST GECODE)
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    ca-certificates \
+    wget \
+    bc \
+    libgl1 \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN wget -qO /tmp/minizinc.tgz https://github.com/MiniZinc/MiniZincIDE/releases/download/2.9.5/MiniZincIDE-2.9.5-bundle-linux-x86_64.tgz \
+    && mkdir -p /opt \
+    && tar -xzf /tmp/minizinc.tgz -C /opt \
+    && mv /opt/MiniZincIDE-2.9.5-bundle-linux-x86_64 /opt/minizinc \
+    && rm /tmp/minizinc.tgz
+ENV PATH="/opt/minizinc/bin:$PATH"
+#################################################################################################
+
+
 # Usefull library for time in bash script
 RUN apt-get install -y bc  
 
--- source/CP/start_CP.sh
+++ source/CP/start_CP.sh
@@ -8,7 +8,10 @@ docker=${1:-}     # --docker if using Docker, empty string otherwise
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" 
 while true; do
     echo -e "\n===== Program Menu ====="
-    echo "1. Model1 (CP) "
+    echo "1. Model1 (CP) Gecode [feasible]"
+    echo "2. Model2 (CP) Chuffed [feasible]"
+    echo "3. Model1 (CP) Gecode [optimal]"
+    echo "4. Model2 (CP) Chuffed [optimal]"
     echo "0. EXIT "
     echo "========================"
 
@@ -22,9 +25,37 @@ while true; do
         read -rp "Enter number of teams (int): " team
         
         start=$(date +%s.%N)
-        # minizinc -D "n=$team" $SCRIPT_DIR/STS_matchVar_first\ column.mzn > output.txt
-        # minizinc --solver Gecode --two-pass -D "n=$team" "$SCRIPT_DIR/STS_matchVar_first column.mzn" > output.txt
         minizinc --solver Gecode -D "n=$team" "$SCRIPT_DIR/STS_matchVar_first column.mzn" > output.txt
+        end=$(date +%s.%N)
+        runtime=$(echo "$end - $start" | bc)
+        python3 $SCRIPT_DIR/minizinc_reader.py $runtime "$docker"
+        rm output.txt
+    elif [[ "$choice" == "2" ]]; then
+        # Ask parameters
+        read -rp "Enter number of teams (int): " team
+        
+        start=$(date +%s.%N)
+        minizinc --solver Chuffed -D "n=$team" "$SCRIPT_DIR/STS_matchVar_first column.mzn" > output.txt
+        end=$(date +%s.%N)
+        runtime=$(echo "$end - $start" | bc)
+        python3 $SCRIPT_DIR/minizinc_reader.py $runtime "$docker"
+        rm output.txt
+    elif [[ "$choice" == "3" ]]; then
+        # Ask parameters
+        read -rp "Enter number of teams (int): " team
+        
+        start=$(date +%s.%N)
+        minizinc --solver Gecode -D "n=$team" "$SCRIPT_DIR/STS_matchVar_first column_optimized.mzn" > output.txt
+        end=$(date +%s.%N)
+        runtime=$(echo "$end - $start" | bc)
+        python3 $SCRIPT_DIR/minizinc_reader.py $runtime "$docker"
+        rm output.txt
+    elif [[ "$choice" == "4" ]]; then
+        # Ask parameters
+        read -rp "Enter number of teams (int): " team
+        
+        start=$(date +%s.%N)
+        minizinc --solver Chuffed -D "n=$team" "$SCRIPT_DIR/STS_matchVar_first column_optimized.mzn" > output.txt
         end=$(date +%s.%N)
         runtime=$(echo "$end - $start" | bc)
         python3 $SCRIPT_DIR/minizinc_reader.py $runtime "$docker"
